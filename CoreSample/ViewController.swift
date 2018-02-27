ed4db065514275592b1982aa8805d89a4b42c90d
homahi <homahi0128@gmail.com>
2018-02-27 18:24:13 +0900

    init

--- CoreSample/ViewController.swift
+++ CoreSample/ViewController.swift
@@ -7,19 +7,75 @@
 //
 
 import UIKit
+import CoreLocation
+import CoreMotion
+import RxSwift
+import RxCoreMotion
+import RxCoreLocation
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    let locationManager = CLLocationManager()
+    let pedometer = CMPedometer()
+    let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.allowsBackgroundLocationUpdates = true
+        locationManager.pausesLocationUpdatesAutomatically = false
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+        
+        UIDevice.current.isBatteryMonitoringEnabled = true
+        
+        let locationObservable = locationManager.rx.didUpdateLocations.map { (manager, locations) -> GPS in
+            let lastLocation = locations.last
+            let gps = GPS(ts: Date(), lat: (lastLocation?.coordinate.latitude)!, lon: (lastLocation?.coordinate.longitude)!, alt: (lastLocation?.altitude)!, acc: (lastLocation?.verticalAccuracy)!)
+            return gps
+        }.throttle(3.0, latest: false, scheduler: MainScheduler.instance)
+
+        let batteryObservable = Observable<Int>.interval(3.0, scheduler: MainScheduler.instance).map({ _ -> Battery in
+            return Battery(ts: Date(), level: UIDevice.current.batteryLevel * 100)
+        })
+
+        Observable<Int>.interval(1, scheduler: MainScheduler.instance)
+            .map({ _ in 1})
+            .subscribe{
+                self.locationManager.requestLocation()
+        }.disposed(by: disposeBag)
+        
+        // CoreMotionのStreamを作る
+        
+        let pedometerObservable = pedometer.rx.pedometer(from: Date()).debug("pedometer")
+
+        Observable.zip(locationObservable, batteryObservable ).debug("zip")
+            .throttle(3, latest: false, scheduler: MainScheduler.instance)
+            .subscribe(onNext: { _ in
+            
+        }).disposed(by: disposeBag)
+        
+        // それらを一定間隔で固めて送信するStreamを作る
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
     }
 
 
 }
 
+struct GPS: Decodable {
+    var ts: Date
+    var lat: CLLocationDegrees
+    var lon: CLLocationDegrees
+    var alt: CLLocationDegrees
+    var acc: Double
+}
+struct Battery : Decodable{
+    var ts: Date
+    var level: Float
+}
---
+++ Podfile
@@ -0,0 +1,10 @@
+source 'https://github.com/CocoaPods/Specs.git'
+platform :ios, "9.3"
+target 'CoreSample' do
+  use_frameworks!
+  pod 'RxSwift'
+  pod 'RxCocoa'
+  pod 'RxCoreLocation'
+  pod 'RxCoreMotion'
+  pod 'Moya/RxSwift'
+end
